7f178328875e1089822c811193d89cf5dfcc10e3
RayanUniversitet <rayan.universitet@gmail.com>
2024-03-26 13:58:21 +0100

    header made

--- static/js/header_base.js
+++ static/js/header_base.js
@@ -2,7 +2,7 @@ hamburger_btn = document.querySelector('#hamburger_btn')
 header_slide_in_background = document.querySelector('.header_slide_in_background')
 header_slide_in_container = document.querySelector('.header_slide_in_container')
 
-hamburger_btn.addEventListener("click", function() {
+hamburger_btn.addEventListener('click', function() {
     if (header_slide_in_background.classList.contains('show')) {
         header_slide_in_background.classList.remove('show')
         header_slide_in_container.classList.remove('show')
@@ -10,7 +10,6 @@ hamburger_btn.addEventListener("click", function() {
         header_slide_in_background.classList.add('show')
         header_slide_in_container.classList.add('show')
     }
-    print('awdawdaw')
 });
 
 header_slide_in_background.addEventListener("click", function() {
