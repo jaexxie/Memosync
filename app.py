f483f3f200f4b5a7f0a9ee45cc61a3c7a07444e4
RayanUniversitet <rayan.universitet@gmail.com>
2024-04-05 02:20:15 +0200

    restrictions created for specific routes

--- app.py
+++ app.py
@@ -3,72 +3,114 @@ from db import make_db_connection
 
 @route('/register')
 def register():
-    return template('register')
+    # Make sure they aren't already logged in
+    logged_in_cookie = request.get_cookie('loggedIn')
+    if logged_in_cookie:
+        return redirect('/overview')
+    else:
+        return template('register')
 
 @route('/register/add/user', method=['post'])
 def add_user():
     '''
     This function adds user to the database after they have submited their information on the registration page.
     '''
-    try:
-        # Database Connection
-        db = make_db_connection()
-        cursor = db.cursor()
-        if request.method == 'POST':
-            first_name = request.forms.get('first_name')
-            last_name = request.forms.get('last_name')
-            email = request.forms.get('email')
-            password = request.forms.get('password')
+    # Make sure they aren't already logged in
+    logged_in_cookie = request.get_cookie('loggedIn')
+    if logged_in_cookie:
+        return redirect('/overview')
+    else:
+        try:
+            # Database Connection
+            db = make_db_connection()
+            cursor = db.cursor()
+            if request.method == 'POST':
+                first_name = request.forms.get('first_name')
+                last_name = request.forms.get('last_name')
+                email = request.forms.get('email')
+                password = request.forms.get('password')
 
-            cursor.execute('insert into user_info (name, lastname, email, password) values (%s, %s, %s, %s)', (first_name, last_name, email, password))
-            db.commit()
+                cursor.execute('insert into user_info (name, lastname, email, password) values (%s, %s, %s, %s)', (first_name, last_name, email, password))
+                db.commit()
 
-            return redirect('/login')
-    finally:
-        # Closing Database connection after it's been used
-        cursor.close()
-        db.close()
+                return redirect('/login')
+        finally:
+            # Closing Database connection after it's been used
+            cursor.close()
+            db.close()
 
 @route('/login', method=['GET', 'POST'])
 def login():
     '''
     This function shows user shows user the loginpage and it logges them in if they entered the right info
     '''
-    try:
-        # Database Connection
-        db = make_db_connection()
-        cursor = db.cursor()
-        if request.method == 'POST':
-            email = request.forms.get('email')
-            password = request.forms.get('password')
+    # Make sure they aren't already logged in
+    logged_in_cookie = request.get_cookie('loggedIn')
+    if logged_in_cookie:
+        return redirect('/overview')
+    else:
+        try:
+            # Database Connection
+            db = make_db_connection()
+            cursor = db.cursor()
+            if request.method == 'POST':
+                email = request.forms.get('email')
+                password = request.forms.get('password')
 
-            cursor.execute('select * from user_info where email=%s and password=%s', (email, password))
-            user = cursor.fetchone()
+                cursor.execute('select * from user_info where email=%s and password=%s', (email, password))
+                user = cursor.fetchone()
 
-            if user:
-                # Set a cookie indicating the user is logged in
-                response.set_cookie('loggedIn', user[0], secret=int(user[0]))
-                return redirect('/overview')
+                if user:
+                    # Set a cookie indicating the user is logged in
+                    response.set_cookie('loggedIn', str(user[0]))
+                    return redirect('/overview')
+                else:
+                    return template('login', email_or_password_wrong=True)
             else:
-                return template('login', email_or_password_wrong=True)
-        else:
-            return template('login', email_or_password_wrong=False)
-    finally:
-        # Closing Database connection after it's been used
-        cursor.close()
-        db.close()
+                return template('login', email_or_password_wrong=False)
+        finally:
+            # Closing Database connection after it's been used
+            cursor.close()
+            db.close()
 
 @route('/')
 def index():
-    return template('index')
+    # Make sure they aren't already logged in
+    logged_in_cookie = request.get_cookie('loggedIn')
+    if logged_in_cookie:
+        return redirect('/overview')
+    else:
+        return template('index')
 
 @route('/overview')
 def overview():
-    return template('overview')
+    # Make sure they aren't already logged in
+    logged_in_cookie = request.get_cookie('loggedIn')
+    if logged_in_cookie:
+        try:
+            # Database Connection
+            db = make_db_connection()
+            cursor = db.cursor()
+
+            cursor.execute('select * from user_info where id = %s', (logged_in_cookie))
+            user_info = cursor.fetchone()
+
+            return redirect('/overview')
+        finally:
+            # Closing Database connection after it's been used
+            cursor.close()
+            db.close()
+    else:
+        return redirect('/')
 
 @route('/to_do_list')
 def to_do_list():
-    return template('to_do_list')
+    # Make sure they aren't already logged in
+    logged_in_cookie = request.get_cookie('loggedIn')
+    if logged_in_cookie:
+        return template('to_do_list')
+    else:
+        return redirect('/')
 
 @route('/static/<filepath:path>')
 def server_static(filepath):
