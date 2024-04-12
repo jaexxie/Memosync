d80f0367f094b8276910d33502ea8d071cee0646
jxmah <jxmah.23@gmail.com>
2024-04-12 12:26:00 +0200

    commit

--- static/js/popup.js
+++ static/js/popup.js
@@ -1,8 +1,24 @@
 open_popup_btn = document.querySelector('#open_popup_btn')
+popup_container = document.querySelector('.popup_container')
+
+open_popup_btn.addEventListener('click', function () {
+    if (popup_container.classList.contains('show')) {
+        popup_container.classList.remove('show')
+    } else {
+        popup_container.classList.add('show')
+    }
+});
+
+popup_container.addEventListener("click", function () {
+    popup_container.classList.remove('show')
+});
+
+/*
+open_popup_btn = document.querySelector('#open_popup_btn')
 popup_background = document.querySelector('.popup_background')
 popup_container = document.querySelector('.popup_container')
 
-open_popup_btn.addEventListener('click', function() {
+open_popup_btn.addEventListener('click', function () {
     if (popup_background.classList.contains('show')) {
         popup_background.classList.remove('show')
         popup_container.classList.remove('show')
@@ -12,7 +28,7 @@ open_popup_btn.addEventListener('click', function() {
     }
 });
 
-popup_background.addEventListener("click", function() {
+popup_background.addEventListener("click", function () {
     popup_background.classList.remove('show')
     popup_container.classList.remove('show')
-});
+});*/
