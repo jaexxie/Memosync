287346502df2ac1d9edff0080f657553093b46a5
StefanYousif <102787518+StefanYousif@users.noreply.github.com>
2024-04-21 14:09:08 +0200

    Uppdatering av kod

--- static/js/progress-modal.js
+++ static/js/progress-modal.js
@@ -3,15 +3,19 @@ document.addEventListener("DOMContentLoaded", function () {
     const modal = document.querySelector(".modal-container");
     const openModalBtn = document.getElementById("create-btn");
     const closeModalBtn = document.querySelector(".close");
+    const modaloverlay = document.getElementById("modal-overlay");
+    
 
     //Open modal when create-btn i clicked 
     const openModal = () => {
         modal.style.display = "block";
+        modaloverlay.style.display = "block";
     }
 
     //close modal when close btn is clicked
     const closeModal = () => {
         modal.style.display = "none";
+        modaloverlay.style.display = "none";
     }
 
     openModalBtn.addEventListener("click", openModal);
