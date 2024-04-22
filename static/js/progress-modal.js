d4afb493eb4598cb0699ad92bb1ecef455ff4771
StefanYousif <102787518+StefanYousif@users.noreply.github.com>
2024-04-22 21:31:39 +0200

    edit

--- static/js/progress-modal.js
+++ static/js/progress-modal.js
@@ -1,29 +1,31 @@
 
-document.addEventListener("DOMContentLoaded", function () {
-    const modal = document.querySelector(".modal-container");
-    const openModalBtn = document.getElementById("create-btn");
-    const closeModalBtn = document.querySelector(".close");
-
-    //Open modal when create-btn i clicked 
-    const openModal = () => {
-        modal.style.display = "block";
-    }
-
-    //close modal when close btn is clicked
-    const closeModal = () => {
-        modal.style.display = "none";
-    }
-
-    openModalBtn.addEventListener("click", openModal);
-
-    closeModalBtn.addEventListener("click", closeModal);
-
-    // Close modal when the Esc key is pressed
-    document.addEventListener("keydown", function (e) {
-        if (e.key === "Escape" && modal.style.display !== "none") {
-        closeModal();
-        }
-    });
+const modal = document.querySelector(".modal-container");
+const openModalBtn = document.getElementById("create-btn");
+const closeModalBtn = document.querySelector(".close");
+const modaloverlay = document.querySelector(".modal-overlay");
 
 
+//Open modal when create-btn i clicked 
+//const openModal = () => {
+   // modal.classList.add('show');
+   // modaloverlay.classList.add('show');
+//}
+
+openModalBtn.addEventListener('click', function () {
+    modal.style.display = "block";
+    modaloverlay.style.display = "block";
+})
+
+//close modal when close btn is clicked
+closeModalBtn.addEventListener("click", function () {
+    modal.style.display = "none";
+    modaloverlay.style.display = "none";
 });
+
+// Close modal when the Esc key is pressed
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" && modal.style.display !== "none") {
+    closeModal();
+    }
+});
+
