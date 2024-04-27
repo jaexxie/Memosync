b790de84e464f132783f2dbf53434ed464e7492d
StefanYousif <102787518+StefanYousif@users.noreply.github.com>
2024-04-27 18:13:14 +0200

    progress-table
    conflicts resolved

--- static/js/progress_modal.js
+++ static/js/progress_modal.js
@@ -82,50 +82,6 @@ document.addEventListener('DOMContentLoaded', function () {
         select.style.backgroundColor = backgroundColor;
     };
 
-
-    function updateStatus(taskId, newStatus) {
-        // Send an AJAX request to update_status route
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", "/update_status", true);
-        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                console.log(xhr.responseText); // Output response from the server
-            }
-        };
-        xhr.send("task_id=" + taskId + "&new_status=" + newStatus);
-    }
-    
-    aa7b5c8bb7408d521e0e34d1ee22a8930107b426
-    // Add an event listener to each select element
-    document.querySelectorAll('.status-col').forEach(select => {
-        select.addEventListener('change', function () {
-            // Get the project ID from the select element's ID
-            const projectId = this.id.split('-')[1];
-            // Get the updated status value
-            const status = this.value;
-            // Send an AJAX request to update the status
-            fetch('/update_status', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ projectId, status })
-            })
-                .then(response => {
-                    // Handle the response if needed
-                })
-                .catch(error => {
-                    console.error('Error:', error);
-                });
-        });
-    });
-
-
-    a7b5c8bb7408d521e0e34d1ee22a8930107b426
-
-
-    aa7b5c8bb7408d521e0e34d1ee22a8930107b426
 });
 
 
