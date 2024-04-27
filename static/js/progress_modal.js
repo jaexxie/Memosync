e05e3ccd44906feb05336951f8ca33bde22da431
jxmah <jxmah.23@gmail.com>
2024-04-27 15:07:27 +0200

    commit css (and some js styling)

--- static/js/progress_modal.js
+++ static/js/progress_modal.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
     modal = document.querySelector(".modal-container");
     openModalBtn = document.querySelector("#create-btn");
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
     form = document.getElementById("task-form");
 
 
-    openModalBtn.addEventListener("click", function() {
+    openModalBtn.addEventListener("click", function () {
         if (modalOverlay.classList.contains("show")) {
             modal.classList.remove("show");
             modalOverlay.classList.remove("show");
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    modalOverlay.addEventListener("click", function() {
+    modalOverlay.addEventListener("click", function () {
         modal.classList.remove("show");
         modalOverlay.classList.remove("show");
         //modalen finns fortfarande i backgrunden (osynlig)
@@ -40,7 +40,7 @@ document.addEventListener('DOMContentLoaded', function() {
             //lösningen funkar ej 
             modal.style.display = "none";
             modalOverlay.style.display = "none";
-            
+
             form.reset();
         }
     });
@@ -52,29 +52,29 @@ document.addEventListener('DOMContentLoaded', function() {
 
     selects.forEach(function (select) {
         //uppdatera backgrundsfärgen när select värde ändras
-        select.addEventListener("change", function() {
+        select.addEventListener("change", function () {
             updateBackgroundColor(this);
         });
-        
+
         updateBackgroundColor(select);
     })
-    
+
     function updateBackgroundColor(select) {
         var selectedOption = select.value;
         var backgroundColor;
 
         //if (selectedOption === "not_started") {
-           // backgroundColor = "#FFA07A"; } else if ...
-        
-        switch(selectedOption) {
+        // backgroundColor = "#FFA07A"; } else if ...
+
+        switch (selectedOption) {
             case "not_started":
-                backgroundColor = "#FFA07A";
+                backgroundColor = "#cf7a74";
                 break;
             case "in_progress":
-                backgroundColor = "#FFFF99";
+                backgroundColor = "#f0eeb1";
                 break;
             case "completed":
-                backgroundColor = "#90EE90";
+                backgroundColor = "#db95bc";
                 break;
             default:
                 backgroundColor = "#f8f8f8";
@@ -83,31 +83,31 @@ document.addEventListener('DOMContentLoaded', function() {
         select.style.backgroundColor = backgroundColor;
     };
 
-        // Add an event listener to each select element
-        document.querySelectorAll('.status-col').forEach(select => {
-            select.addEventListener('change', function() {
-                // Get the project ID from the select element's ID
-                const projectId = this.id.split('-')[1];
-                // Get the updated status value
-                const status = this.value;
-                // Send an AJAX request to update the status
-                fetch('/update_status', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({ projectId, status })
-                })
+    // Add an event listener to each select element
+    document.querySelectorAll('.status-col').forEach(select => {
+        select.addEventListener('change', function () {
+            // Get the project ID from the select element's ID
+            const projectId = this.id.split('-')[1];
+            // Get the updated status value
+            const status = this.value;
+            // Send an AJAX request to update the status
+            fetch('/update_status', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ projectId, status })
+            })
                 .then(response => {
                     // Handle the response if needed
                 })
                 .catch(error => {
                     console.error('Error:', error);
                 });
-            });
         });
+    });
 
-    
+
 });
 
 
