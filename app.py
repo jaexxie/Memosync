13c9c833520d386aa85317d5846ae6279830503c
RayanUniversitet <151962882+RayanDoski@users.noreply.github.com>
2024-05-06 20:52:53 +0200

    terminate account functionality added

--- app.py
+++ app.py
@@ -268,7 +268,7 @@ def update_user_info():
                 # update the user's profile picture in the database
                 cursor.execute("UPDATE memosync.user_info SET profile_picture = %s WHERE id = %s", (filename, logged_in_cookie))
                 db.commit()
-                
+
                 # save the image to the specified directory
                 filename = image.filename
                 filepath = os.path.join('static/pic/user_profile_pictures', filename)
@@ -305,6 +305,29 @@ def delete_profile_picture():
             cursor = db.cursor()
 
             cursor.execute('update user_info set profile_picture = %s where id = %s', ('default.jpeg', logged_in_cookie))
+            db.commit()
+
+            return redirect(request.get_header('Referer'))
+        finally:
+            # close database connection
+            cursor.close()
+            db.close()
+    else:
+        # if the user is not logged in, redirect to the home page
+        return redirect('/')
+    
+@route('/delete/my/account', method=['GET', 'POST'])
+def delete_my_account():
+    # checks if the user is already logged in by checking the 'loggedIn' cookie
+    logged_in_cookie = request.get_cookie('loggedIn')
+
+    if logged_in_cookie:
+        try:
+            # connect to the database
+            db = make_db_connection()
+            cursor = db.cursor()
+
+            cursor.execute('delete from user_info where id = %s', (logged_in_cookie))
             db.commit()
 
             return redirect(request.get_header('Referer'))
